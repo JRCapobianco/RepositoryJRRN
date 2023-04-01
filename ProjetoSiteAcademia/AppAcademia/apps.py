9228b2b33ce895cf019c1f8fab0ddc3469566f61
João Rodrigues C. Ramalho <joaocapobiancoramalho@outlook.com>
2023-04-01 19:46:36 -0300

    Commit 0.4 Desafio

--- ProjetoSiteAcademia/AppAcademia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-#Aplicativo Registrado como 'AppAcademia' // Aplication registered as 'AppAcademia'.
-class AppacademiaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppAcademia'
